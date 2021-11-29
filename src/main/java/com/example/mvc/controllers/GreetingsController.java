34208feccf9c9591d78227fbbb1e7a9e09c2a5c1
ddKirill <cfaleev@yandex.ru>
2021-11-29 15:43:44 +0300

    work version1

--- src/main/java/com/example/mvc/controllers/GreetingsController.java
+++ src/main/java/com/example/mvc/controllers/GreetingsController.java
@@ -2,17 +2,26 @@ package com.example.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingsController {
 
     @RequestMapping("/firstpage")
     public String firstpage(@RequestParam(name="name", required=false,
-            defaultValue="World") String name, Model model)
+            defaultValue="World") String name, Map<String, Object> model)
     {
-        model.addAttribute("name", name);
+        model.put("name", name);
           return "firstpage";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        model.put("some", "Hello people");
+        return "main";
     }
 }
